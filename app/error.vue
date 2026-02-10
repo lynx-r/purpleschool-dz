2af0c344ae3dadd99d4636f41e49ae89f33f4312
a.popryadukhin <a.popryadukhin@b2b-center.ru>
2026-02-10 18:44:47 +0300

    dz 2

---
+++ app/error.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps<{
+  error: NuxtError;
+}>();
+</script>
+
+<template>
+  <div class="error__wrapper">
+    <h1 class="error__status">{{ error.status }}</h1>
+    <div
+      class="error__message"
+      v-if="error.status === 404"
+    >
+      Страница не найдена, вернитесь на главную страницу
+    </div>
+    <div
+      class="error__message"
+      v-else
+    >
+      {{ error.message }}
+    </div>
+    <ActionButton class="error__button"> На главную </ActionButton>
+  </div>
+</template>
+
+<style scoped>
+.error__wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.error__status {
+  font-weight: 500;
+  font-size: 120px;
+  margin: 0;
+  color: var(--color-black-light);
+}
+
+.error__message {
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 150%;
+  text-align: center;
+  color: var(--color-black-light);
+  margin-bottom: 24px;
+}
+
+.error__button {
+  width: 300px;
+}
+</style>
